de5fd220da4383919abaff0b5e13b485f5b33395
Gajus Kuizinas <g.kuizinas@anuary.com>
2012-10-04 14:42:48 +0000

    Default gitignore added.

--- ay/includes/config.inc.php
+++ ay/includes/config.inc.php
@@ -2,5 +2,5 @@
 return array
 (
 	'xhprof_url'	=> 'https://dev.anuary.com/8d50658e-f8e0-5832-9e82-6b9e8aa940ac/',
-	'pdo'			=> new PDO('mysql:dbname=2012 09 16 xhprof;host=localhost;charset=utf8', 'root', 'BZiwKV3XhvbS'),
+	'pdo'			=> new PDO('mysql:dbname=your_database_name;host=localhost;charset=utf8', 'username', 'password'),
 );

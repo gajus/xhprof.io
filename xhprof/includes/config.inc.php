a49350d8007714e53efadd7eec95afb65a3141c9
Gajus Kuizinas <g.kuizinas@anuary.com>
2012-10-05 22:29:39 +0000

    updated ignore rules.

--- xhprof/includes/config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array
-(
-	'base_url'	=> 'https://dev.anuary.com/1e4d143d-a3ec-5110-80e2-5e5212f93779/',
-	'pdo'		=> new PDO('mysql:dbname=2012 09 16 xhprof;host=localhost;charset=utf8', 'root', 'BZiwKV3XhvbS')
-);

ff4640ded1a1517ed6352b917aae368535531fa3
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-02-04 10:26:00 +0000

    Minor syntax changes.

--- xhprof/includes/bootstrap.inc.php
+++ xhprof/includes/bootstrap.inc.php
@@ -3,14 +3,12 @@ namespace ay\xhprof;
 
 session_start();
 
-if(isset($_GET['ay']['debug']))
-{
-	$_SESSION['ay']['debug']		= !empty($_GET['ay']['debug']);
+if (isset($_GET['ay']['debug'])) {
+	$_SESSION['ay']['debug'] = !empty($_GET['ay']['debug']);
 }
 
-if(isset($_GET['ay']['profiling']))
-{
-	$_SESSION['ay']['profiling']	= !empty($_GET['ay']['profiling']);
+if (isset($_GET['ay']['profiling'])) {
+	$_SESSION['ay']['profiling'] = !empty($_GET['ay']['profiling']);
 }
 
 define('VERSION', '0.0.0');
@@ -38,8 +36,7 @@ set_error_handler('ay\error_exception_handler');
 
 $config	= require BASE_PATH . '/includes/config.inc.php';
 
-if(!isset($config['base_url'], $config['pdo']))
-{
+if (!isset($config['base_url'], $config['pdo'])) {
 	throw new \Exception('XHProf.io is not configured. Refer to /xhprof/includes/config.inc.php.');
 }
 
@@ -51,13 +48,11 @@ require_once BASE_PATH . '/classes/data.php';
 require BASE_PATH . '/classes/model.php';
 require BASE_PATH . '/classes/callgraph.php';
 
-if(\AY\DEBUG && !empty($_SESSION['ay']['profiling']))
-{
+if (\AY\DEBUG && !empty($_SESSION['ay']['profiling'])) {
 	require BASE_PATH . '/includes/profiler.inc.php';
 }
 
-if(filter_has_var(INPUT_POST, 'ay'))
-{
+if (filter_has_var(INPUT_POST, 'ay')) {
 	array_walk_recursive($_POST['ay'], function(&$e){
 		$e	= trim($e);
 	});
@@ -67,8 +62,7 @@ if(filter_has_var(INPUT_POST, 'ay'))
 	$_SESSION['ay']['flash']['input']	= $_POST['ay'];
 }
 
-if(empty($_SESSION['xhprof']['remote_version']))
-{
+if (empty($_SESSION['xhprof']['remote_version'])) {
 	$ch			= curl_init();
 	
 	curl_setopt_array($ch, array(
@@ -83,16 +77,14 @@ if(empty($_SESSION['xhprof']['remote_version']))
 	
 	$version	= json_decode($response, TRUE);
 	
-	if(!empty($version['version']))
-	{
+	if (!empty($version['version'])) {
 		$_SESSION['xhprof']['remote_version']	= $version['version'];
 	}
 	
 	unset($version, $response);
 }
 
-if(!empty($_SESSION['xhprof']['remote_version']) && $_SESSION['xhprof']['remote_version'] != VERSION)
-{
+if (!empty($_SESSION['xhprof']['remote_version']) && $_SESSION['xhprof']['remote_version'] != VERSION) {
 	\ay\message('You are running an out-of-date version of XHProf.io (' . VERSION . '). The <a href="http://xhprof.io/" target="_blank">current version is ' . htmlspecialchars($_SESSION['xhprof']['remote_version']) . '</a>.', \AY\MESSAGE_NOTICE);
 	
 	unset($_SESSION['xhprof']['remote_version']);

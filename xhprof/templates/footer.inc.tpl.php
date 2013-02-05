14dc1c61dc604755e9f304861c866db126b9867a
Gajus Kuizinas <g.kuizinas@anuary.com>
2013-02-05 22:54:49 +0000

    License changed to BSD.

--- xhprof/templates/footer.inc.tpl.php
+++ xhprof/templates/footer.inc.tpl.php
@@ -2,5 +2,5 @@
 namespace ay\xhprof;
 ?>
 <div id="footer">
-	<p><a href="http://xhprof.io/" target="_blank">XHProf.io</a> (v<?=VERSION?>) is developed and maintained by <a href="http://anuary.com/gajus" target="_blank">Gajus Kuizinas</a>.<br>This software is distributed under the <a href="https://github.com/gajus/xhprof.io/blob/master/LICENSE" target="_blank">GNU General Public License</a>.</p>
+	<p><a href="http://xhprof.io/" target="_blank">XHProf.io</a> (v<?=VERSION?>) is developed and maintained by <a href="http://anuary.com/gajus" target="_blank">Gajus Kuizinas</a>.</p>
 </div>
